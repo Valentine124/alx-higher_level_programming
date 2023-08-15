7db139866d79a89dfaff6dc9086bd5f2b9d18769
valentine <kaluvalentine60@gmail.com>
2023-08-14 20:13:08 -0700

    Task 1

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def element_at(my_list,idx):
+    c = 0
+
+    if idx < 0:
+        return None
+
+    for i in my_list:
+        c += 1
+        if idx > c:
+            return None
+        if idx == c - 1:
+            return i
