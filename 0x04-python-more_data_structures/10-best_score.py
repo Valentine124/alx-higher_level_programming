54400955548b3523d44fac4c9ce528126debce01
valentine <kaluvalentine60@gmail.com>
2023-08-19 21:39:52 -0700

    Task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+
+    best = ""
+    score = 0
+
+    for key, val in a_dictionary.items():
+        if val > score:
+            score = val
+            best = key
+
+    return best
