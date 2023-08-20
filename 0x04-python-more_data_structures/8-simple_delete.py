94715d55c31896ef32fd5d5089f7ac138055a441
valentine <kaluvalentine60@gmail.com>
2023-08-19 21:22:16 -0700

    Task 8

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -3,9 +3,9 @@ def simple_delete(a_dictionary, key=""):
     if a_dictionary is None:
         return None
 
-    if key not in a_dictionary.items():
-        return a_dictionary
-
-    del a_dictionary[key]
+    for el, val in a_dictionary.items():
+        if el == key:
+            del a_dictionary[key]
+            return a_dictionary
 
     return a_dictionary
