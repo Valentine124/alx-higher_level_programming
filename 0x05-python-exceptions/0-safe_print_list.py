5e7d014cce5fe86de64b9c6e4a95cb510f01cce2
valentine <kaluvalentine60@gmail.com>
2023-08-28 18:32:48 -0700

    Task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    num = 0
+
+    if my_list is None:
+        return None
+
+    try:
+        for idx in range(0, x):
+            print("{}".format(my_list[idx]), end="")
+            num +=1
+        print()
+    except IndexError:
+        print()
+
+    return num
