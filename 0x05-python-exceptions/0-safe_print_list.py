ee8ca810c87641318a66a256baf2c8175bebe034
valentine <kaluvalentine60@gmail.com>
2023-08-28 18:39:02 -0700

    Task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,7 +8,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         for idx in range(0, x):
             print("{}".format(my_list[idx]), end="")
-            num +=1
+            num += 1
         print()
     except IndexError:
         print()
