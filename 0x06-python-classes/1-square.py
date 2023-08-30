09a8019f45e6b4276c62bd3c74c65d45833af69f
valentine <kaluvalentine60@gmail.com>
2023-08-29 21:31:03 -0700

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This module contains a class Squre
+"""
+class Square:
+    """
+    The class Square creates a squre with
+    size.
+    """
+
+    def __init__(self, size):
+        """
+        The Squre class initializer or constructor
+
+        Args:
+            size (int): The size of the square
+
+        Attributes:
+            size (int): The size.
+        """
+
+        self.__size = size
