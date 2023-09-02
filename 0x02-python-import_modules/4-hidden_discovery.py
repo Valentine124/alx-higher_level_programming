a025c4ada4440793804e251d8b2f96b4307b3e9a
valentine <kaluvalentine60@gmail.com>
2023-09-02 05:33:12 -0700

    Task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-import hidden_4
+if __name__ == '__main__':
+    import hidden_4
 
-d = dir(hidden_4)
+    d = dir(hidden_4)
 
-for name in d:
-    if name[0] == '_' and name[1] == '_':
-        continue
-    print("{}".format(name))
+    for name in d:
+        if name[0] == '_' and name[1] == '_':
+            continue
+        print("{}".format(name))
