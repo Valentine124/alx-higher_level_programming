77a9adbd117cb7366b306553d83d56bd5176f2a5
valentine <kaluvalentine60@gmail.com>
2023-09-12 05:39:20 -0700

    Task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+This module contains one function
+"""
+
+
+def to_json_string(my_obj):
+    """
+    This function returns the json
+    representation of an object
+
+    Args:
+        my_obj - The object
+    """
+
+    return json.dumps(my_obj)
