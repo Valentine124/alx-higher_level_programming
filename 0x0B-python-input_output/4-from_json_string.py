6815f2cc18755517304fd48e6078c6489c03cebd
valentine <kaluvalentine60@gmail.com>
2023-09-12 05:46:17 -0700

    Task 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+This module contains one function
+"""
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    This function converts a json string
+    to a python object
+
+    Args:
+        my_str - The json string
+    """
+
+    return json.load(my_str)
