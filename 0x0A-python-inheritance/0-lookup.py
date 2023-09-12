e09949b3e8caa9ed87be5a0f06b07e4f0a14206a
valentine <kaluvalentine60@gmail.com>
2023-09-11 17:55:21 -0700

    Task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,15 @@
+"""
+This module contains a function
+that lookup an object
+"""
+
+def lookup(obj):
+    """
+    This function prints the attributes
+    and methods of an object
+
+    Args:
+        obj - The object
+    """
+
+    return obj.__dict__
