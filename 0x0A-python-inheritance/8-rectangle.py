a9abcb7ed33490fc190fa46c2b6af3b46ace6500
valentine <kaluvalentine60@gmail.com>
2023-09-14 14:56:32 -0700

    Task 8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""
+This module implements the typr for Rectangle
+"""
+
+
+class Rectangle(BaseGeometry):
+    """
+    This class create the Rectangle
+    inheriting from the BaseGeometry
+
+    Attributes:
+        _Rectangle__width - The rectangle width
+        _Rectangle__height - The rectangle height
+    """
+
+    def __init__(self, width, height):
+        """
+        Rectangle initializer
+
+        Args:
+            width - The width
+            height - The height
+        """
+
+        self._Rectangle__width = width
+        self._Rectangle__height = height
+
+        super().integer_validator("Rectangle", self._Rectangle__width)
+        super().integer_validator("Rectangle", self._Rectangle__height)
