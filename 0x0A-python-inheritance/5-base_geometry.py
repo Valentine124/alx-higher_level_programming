361ec75d9f1df84dbe415f4bac4f22da987359ee
valentine124 <kaluvalentine60@gmail.com>
2023-11-06 16:55:09 -0800

    Task 5

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+This module contains the
+base class of all the
+geometries
+"""
+
+
+class BaseGeometry:
+    """
+    This is the base class
+    of all geometries
+    """
+
+    pass
