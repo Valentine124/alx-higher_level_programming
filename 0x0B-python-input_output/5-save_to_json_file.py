5c751d8439f15a31ca48808af480f4afe4e1cc8b
valentine124 <kaluvalentine60@gmail.com>
2023-11-07 16:27:18 -0800

    Task 5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+This module contains
+a single function
+"""
+
+
+import json
+
+
+def seve_to_json_file(my_obj, filename):
+    """
+    Saves an object to file
+    as json string
+
+    Args:
+        my_obj - the object to save
+        filename - file to save to
+    """
+
+    with open(filename, 'w', encoding="utf-8") as f:
+        json.load(my_obj, f)
