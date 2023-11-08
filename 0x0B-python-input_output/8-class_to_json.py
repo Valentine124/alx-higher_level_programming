a92ab50bb9cbf6acba1da647b02947139561a9a1
valentine124 <kaluvalentine60@gmail.com>
2023-11-07 20:31:36 -0800

    Task 8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+This module contains
+a single function
+"""
+
+
+def class_to_json(obj):
+    """
+    returns the dictionary
+    description of an object
+
+    Args:
+        obj - the object
+    """
+
+    return dict(obj)
