b9b70363d86b084cd687a75c353fd06cfea04cde
valentine124 <kaluvalentine60@gmail.com>
2023-11-07 16:38:35 -0800

    Task 5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -20,4 +20,6 @@ def seve_to_json_file(my_obj, filename):
     """
 
     with open(filename, 'w', encoding="utf-8") as f:
-        json.dump(my_obj, f)
+        str = json.dumps(my_obj)
+
+        wr = f.write(str)
