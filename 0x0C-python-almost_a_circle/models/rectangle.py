26145c9434d2f44e778e259f31997372796f8a3c
Valebtine124 <kaluvalentine60@gmail.com>
2023-11-14 21:03:30 -0800

    Task 3

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -25,10 +25,10 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
 
-        width(width)
-        height(height)
-        x(x)
-        y(y)
+        self.__width = width
+        self.__heght = height
+        self.__x = x
+        self.__y = y
 
     @property
     def width(self):
