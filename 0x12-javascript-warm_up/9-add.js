383002a90f86a1372fc42bc5ac31eef3fb24272d
valentine124 <kaluvalentine60@gmail.com>
2024-01-08 20:01:32 -0800

    Task 9

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+function add (a, b) {
+  return a + b;
+}
+
+const first = Number(process.argv[2]);
+const second = Number(process.argv[3]);
+
+const result = add(first, second);
+console.log(result);
