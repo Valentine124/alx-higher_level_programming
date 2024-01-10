e73f4271b56616cf3035ff9f68669df83a4d50d8
valentine124 <kaluvalentine60@gmail.com>
2024-01-10 10:28:03 -0800

    Task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -11,9 +11,18 @@ if (args.length < 3) {
   let secondLargest = 0;
 
   for (let i = 2; i < args.length; i++) {
+    let flag = 0;
+
     if (Number(args[i]) > largest) {
+      flag = 1;
       secondLargest = largest;
       largest = Number(args[i]);
+    }
+
+    if (!flag) {
+      if (Number(args[i]) > secondLargest) {
+        secondLargest = Number(args[i]);
+      }
     }
   }
 
