a84d22e0bd53c8685dbcf3564af234cd6b9ae7c9
valentine124 <kaluvalentine60@gmail.com>
2024-01-09 18:13:04 -0800

    Task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const Square1 = require('./5-square');
+
+module.exports = class Square extends Square1 {
+  charPrint (c) {
+    if (c === undefined) this.print();
+    else {
+      let outer = 0;
+      let inner = 0;
+
+      while (outer < this.height) {
+        let str = '';
+
+        while (inner < this.width) {
+          str += 'C';
+          inner += 1;
+        }
+
+        inner = 0;
+        console.log(str);
+        outer += 1;
+      }
+    }
+  }
+};
