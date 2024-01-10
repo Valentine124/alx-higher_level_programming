d2054b60960d63ed81a1c98a9ba8637bbccc0f57
valentine124 <kaluvalentine60@gmail.com>
2024-01-09 17:27:05 -0800

    Task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,40 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      if (w === undefined || h === undefined) return;
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let outer = 0;
+    let inner = 0;
+
+    while (outer < this.height) {
+      let str = '';
+
+      while (inner < this.width) {
+        str += 'X';
+        inner += 1;
+      }
+
+      inner = 0;
+      console.log(str);
+      outer += 1;
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
