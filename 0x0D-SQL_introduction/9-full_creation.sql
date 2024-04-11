e94581b2ebe36a1d45cc90b3d3215f75cebedf22
valentine124 <kaluvalentine60@gmail.com>
2024-04-11 14:37:14 -0700

    Task 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- Creates a table and add multiple rows
-CREATE TABLE IF NOT EXISTS secomd_table (id INT, name VARCHAR(256)
+CREATE TABLE IF NOT EXISTS secomd_table (id INT, name VARCHAR(256),
 	score INT);
 INSERT INTO second_table(id, name, score) VALUES(1, 'John', 10);
 INSERT INTO second_table(id, name, score) VALUES(2, 'Alex', 3);
