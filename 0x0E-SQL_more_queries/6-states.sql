38d7f268bcce5139c9fa1d5ad5757662b92d1a1c
valentine124 <kaluvalentine60@gmail.com>
2024-04-14 16:43:49 -0700

    Task 6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,5 @@
+-- Creates a database and a table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (id INT NOT NULL IDENTITY PRIMARY KEY,
+	name VARCHAR(256));
