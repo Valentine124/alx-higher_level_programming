076c214b421c230ed0fd7876e5eeb9e1b8253e45
valentine124 <kaluvalentine60@gmail.com>
2024-04-14 15:43:12 -0700

    Task 2

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- Creates a database and a user
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS user_0d_2
+	IDENTIFIED WITH mysql_native_password BY "user_0d_2_pwd";
+GRANT SELECT ON hbtn_0d_2.* TO "user_0d_2"@"localhost";
