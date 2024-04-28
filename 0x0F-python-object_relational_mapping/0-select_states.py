cb52214fbb7f810fce423c0b8123e2dc713e89a4
valentine124 <kaluvalentine60@gmail.com>
2024-04-28 16:14:23 -0700

    Task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,15 +7,16 @@ data from a table
 
 
 if __name__ == '__main__':
-	import MySQLdb
-	from sys import argv
+    import MySQLdb
+    from sys import argv
 
-	db = MySQLdb.connect(host='localhost', user=argv[1], passwd=argv[2], db=argv[3])
-	cu = db.cursor()
+    db = MySQLdb.connect(host='localhost', user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cu = db.cursor()
 
-	cu.execute("SELECT * FROM states")
+    cu.execute("SELECT * FROM states")
 
-	states = cu.fetchall()
+    states = cu.fetchall()
 
-	for n in states:
-		print(n)
+    for n in states:
+        print(n)
