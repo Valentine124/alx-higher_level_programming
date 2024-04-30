f2817e27f20939864e7ed0102069f60d9f228663
valentine124 <kaluvalentine60@gmail.com>
2024-04-30 08:17:46 -0700

    Task 6

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""
+Contains The definition of
+the state module and the Base
+"""
+
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    This class defines the states
+    table in the database
+    """
+
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+
+    def __repr__(self):
+        return f"State(name='{self.name}')"
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
