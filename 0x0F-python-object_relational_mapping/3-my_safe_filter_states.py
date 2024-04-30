3cf4d52680648db79ef90193e9a64dae50dd303f
valentine124 <kaluvalentine60@gmail.com>
2024-04-30 03:40:02 -0700

    Task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+Contains code that filters
+a database base on some
+arguement
+"""
+
+
+if __name__ == '__main__':
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(host='localhost', user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+
+    n = argv[4]
+
+    cur.execute("SELECT * FROM states WHERE name = BINARY %s;", (n, ))
+
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
