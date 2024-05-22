d1bf7168ffdb40418790fbf214125b85f3632ebe
Valentine124 <kaluvalentine60@gmail.com>
2024-05-21 21:06:03 -0700

    Task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 
 let PATH = process.argv[2];
 
-fs.readFile(PATH, function (err, data) {
+fs.readFile(PATH, 'utf-8', function (err, data) {
 	if (err) {
 		console.error(err);
 		return;
