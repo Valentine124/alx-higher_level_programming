64863c31666c34984ceadecc4045f3c0c6ff7a63
Valentine124 <kaluvalentine60@gmail.com>
2024-07-31 05:26:42 +0100

    Task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const fs = require("fs");
+const process = require("process");
+
+let PATH = process.argv[2];
+
+fs.readFile(PATH, 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log(data)
+});
